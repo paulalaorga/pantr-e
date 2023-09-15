361c78460faddfcfe1917c9a86c6c8ed8f737320
paula <paulalaorga@gmail.com>
2023-09-15 22:44:47 +1000

    JS structure

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
- 
+//Fetch API
+
+
+const searchInput 
+const searchButton
+const clearButton 
+
+const recipeList 
+const selectedRecipeList 
+const ingredientList 
+const shoppingList 
+
+
+const selectedRecipes = []; // My Menu array
+const shoppingListItems = []; // My Shopping List
+
+// Event listener for the search button and clear button
+
+ // Functions to create and display list of recipes
+
+// Functions to select and display My Menu
+
+// Function to fetch and display ingredients for each recipe
+
+// Function to extract the ingredients from the recipe object
+// Change to array iterator method
+
+// Functions to create and display the list of ingredients per recipe
+
+// Function to add selected ingredients to the shopping list
+
+// Function to display the shopping list
